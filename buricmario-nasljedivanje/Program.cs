f8e20d71eca8b0b446388c97cb3964c05b5bae83
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-10-13 10:55:37 +0200

    Add files via upload

---
+++ buricmario-nasljedivanje/Program.cs
@@ -0,0 +1,61 @@
+﻿using buricmario_nasljedivanje;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmario_nasljedivanje
+{
+    interface IControl
+
+    {
+        void Metoda();
+        int Brojac{ get; set; }
+    }
+
+
+    class ListBox : IControl
+    {
+        private int brojac;
+        public void Metoda()
+        {
+            Console.WriteLine("Metoda List");
+        }
+        public int Brojac
+        {
+            get { return brojac; }
+            set { brojac = value; }
+        }
+    }
+    
+    class ComboBox : IControl
+    {
+        public int Brojac { get; set; }
+
+        public void Metoda()
+        {
+
+        }
+    }
+    
+   
+
+    }
+class Program
+{
+    static void Main(string[]args)
+    {
+        ListBox x = new ListBox();
+        ComboBox y = new ComboBox();
+        x.Metoda();
+        y.Metoda();
+    }
+
+}
+
+
+
+    
+    
+
